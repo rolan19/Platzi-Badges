dc880bd349517e44dd1b0270cd1c1c6bed0dd0a9
rolan19 <rolando19jobs@gmail.com>
2020-07-21 21:09:59 -0500

    Home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import BadgeNew from '../pages/BadgeNew';
-import BadgeDetails from '../pages/BadgeDetailsContainer';
-import BadgeEdit from '../pages/BadgeEdit';
-import Badges from '../pages/Badges';
-import Layout from './Layout';
-import NotFound from '../pages/NotFound';
+import BadgeNew from "../pages/BadgeNew";
+import BadgeDetails from "../pages/BadgeDetailsContainer";
+import BadgeEdit from "../pages/BadgeEdit";
+import Badges from "../pages/Badges";
+import Layout from "./Layout";
+import NotFound from "../pages/NotFound";
+import Home from "../pages/Home";
 
 const App = () => {
-   return (
-      <BrowserRouter>
-         <Layout>
-            <Switch>
-               <Route exact path="/badges" component={Badges} />
-               <Route exact path="/badges/new" component={BadgeNew} />
-               <Route exact path="/badges/:badgeId" component={BadgeDetails} />
-               <Route
-                  exact
-                  path="/badges/:badgeId/edit"
-                  component={BadgeEdit}
-               />
-               <Route component={NotFound} />
-            </Switch>
-         </Layout>
-      </BrowserRouter>
-   );
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/badges" component={Badges} />
+          <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
 export default App;
