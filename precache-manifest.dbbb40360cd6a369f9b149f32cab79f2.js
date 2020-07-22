8c21970fcfd7f378d8fdaa4a646aba17c9dd4ed7
rolan19 <rolando19jobs@gmail.com>
2020-07-21 21:07:08 -0500

    Updates

---
+++ precache-manifest.dbbb40360cd6a369f9b149f32cab79f2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b07f9a7c53e06e55408f",
+    "url": "/Platzi-Badges/static/css/main.e16b711f.chunk.css"
+  },
+  {
+    "revision": "b07f9a7c53e06e55408f",
+    "url": "/Platzi-Badges/static/js/main.b07f9a7c.chunk.js"
+  },
+  {
+    "revision": "ca79e1d267b0de3bbf82",
+    "url": "/Platzi-Badges/static/js/runtime~main.ca79e1d2.js"
+  },
+  {
+    "revision": "7e0e25d92ee8b2da7a53",
+    "url": "/Platzi-Badges/static/css/2.396dbfa1.chunk.css"
+  },
+  {
+    "revision": "7e0e25d92ee8b2da7a53",
+    "url": "/Platzi-Badges/static/js/2.7e0e25d9.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Platzi-Badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Platzi-Badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Platzi-Badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Platzi-Badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "48c7d5f3bdc3eaf99b4d8d9566bad645",
+    "url": "/Platzi-Badges/index.html"
+  }
+];
